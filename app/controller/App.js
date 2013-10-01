93a41efa240bd998893a7de5f7bb926e20cb8d97
nico <ng@linuxusers.com.ar>
2013-10-01 00:57:48 -0300

    Adding Nested Menu Support

--- app/controller/App.js
+++ app/controller/App.js
@@ -1,39 +1,39 @@
 Ext.define('SlideNav.controller.App',{
-				extend: 'Ext.app.Controller',    
-				config:{
-								refs:{
-												main : 'main',
-												navigation : 'navigation',
-												
-												navBtn : 'button[name="nav_btn"]'
-								},
-				
-								control : {
-												navBtn : {
-																tap : 'toggleNav'
-												},
-												
-												navigation : {
-																itemtap : function(list, index, target, record){
-																				this.toggleNav();
-																}
-												}
-								}
-				},
-				
-				/**
-				 * Toggle the slide navogation view
-				 */
-				toggleNav : function(){
-								var me = this,
-								mainEl = me.getMain().element;
-								
-								if (mainEl.hasCls('out')) {
-												mainEl.removeCls('out').addCls('in'); 
-												me.getMain().setMasked(false);
-								} else {
-												mainEl.removeCls('in').addCls('out');  
-												me.getMain().setMasked(true);
-								}
+	extend: 'Ext.app.Controller',
+	config:{
+		refs:{
+		main : 'main',
+			navigation : 'nested-menu',
+			navBtn : 'button[name="nav_btn"]'
+		},
+
+		control : {
+			navBtn : {
+				tap : 'toggleNav'
+			},
+
+			navigation : {
+				leafitemtap: function(that, list, index, target, record) {
+					this.toggleNav();
 				}
+			}
+		}
+	},
+
+	/**
+	 * Toggle the slide navogation view
+	 */
+	toggleNav: function() {
+		var me = this,
+			main = me.getMain(),
+			mainEl = main.element;
+
+		if (mainEl.hasCls('out')) {
+			mainEl.removeCls('out').addCls('in');
+			main.setMasked(false);
+		} else {
+			mainEl.removeCls('in').addCls('out');
+			main.setMasked(true);
+		}
+	}
 });
--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -1,21 +1,21 @@
 Ext.define('SlideNav.view.Viewport', {
-				extend: 'Ext.Container',
-				xtype: 'app_viewport',
-				requires: [
-				'Ext.TitleBar'
-				],
-				config: {
-								fullscreen: true,
-								layout: 'hbox',
-								items : [{
-												xtype : 'main',
-												cls: 'slide',
-												
-												// Needed to fit the whole content
-												width: '100%'												
-								}, {
-												xtype : 'navigation',
-												width : 250
-								}]
-				}
+	extend: 'Ext.Container',
+	xtype: 'app_viewport',
+	requires: [
+		'Ext.TitleBar'
+	],
+
+	config: {
+		layout: 'hbox',
+		items: [
+			{
+				xtype: 'main',
+				cls: 'slide',
+				width: '100%'
+			}, {
+				xtype: 'nested-menu',
+				width: 200
+			}
+		]
+	}
 });
